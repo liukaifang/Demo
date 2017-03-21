34496ae38a50c799814ea53edb5aa000ea5d15ee
liukiafang <673166431@qq.com>
2017-03-21 14:32:20 +0800

    ceshi

---
+++ src/at/home/thread/Demo3.java
@@ -0,0 +1,16 @@
+package at.home.thread;
+
+/**
+ * Created by 01308370 on 2017/3/21.
+ */
+public class Demo3 {
+    public static void main(String arg[]){
+        System.out.println("****************");
+        System.out.println("****************");
+        System.out.println("****************");
+        System.out.println("****************");
+        System.out.println("****************");
+        System.out.println("****************");
+
+    }
+}
