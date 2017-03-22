fd2a8737da9da6ef819a7fe5e4072f8bd24da0ec
liukiafang <673166431@qq.com>
2017-03-22 09:52:02 +0800

    test66666

--- src/at/home/thread/CopyOfdemo6.java
+++ src/at/home/thread/CopyOfdemo6.java
@@ -4,6 +4,13 @@ public class CopyOfdemo6 {
 
 	public static void main(String[] args) {
 		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		
+		
+		
 	}
 
 }
