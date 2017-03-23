248edf419e56544832547f5435044f626d2ee0af
liukiafang <673166431@qq.com>
2017-03-23 09:41:52 +0800

    Test

--- src/at/home/thread/demo6.java
+++ src/at/home/thread/demo6.java
@@ -4,6 +4,13 @@ public class demo6 {
 
 	public static void main(String[] args) {
 		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
+		System.out.println("************");
 	}
 
 }
