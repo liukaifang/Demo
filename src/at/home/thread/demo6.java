1d85f8008d50ff966ebe701bef532ef047f8f67f
liukiafang <673166431@qq.com>
2017-03-23 10:02:43 +0800

    Test

--- src/at/home/thread/demo6.java
+++ src/at/home/thread/demo6.java
@@ -3,14 +3,9 @@ package at.home.thread;
 public class demo6 {
 
 	public static void main(String[] args) {
-		System.out.println("************");
-		System.out.println("************");
+		System.out.println("*****6*******");
+		System.out.println("****7********");
 		System.out.println("******6******");
-		System.out.println("****6********");
-		System.out.println("******6******");
-		System.out.println("*****6*******");
-		System.out.println("*****6*******");
-		System.out.println("*****6*******");
 		
 		
 	}
